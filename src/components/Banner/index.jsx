0fe015de993dd288a7c2d73f061da0fef6e78df9
Nohavye <noha.poncelet@gmail.com>
2023-08-17 18:47:58 +0200

    Composant Banner: Gestion de la hauteur fonction de la route + Affichage du titre (h1) fonction dela prop 'text'

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,21 @@
 import './styles.scss'
+import { useLocation } from 'react-router-dom'
 
 function Component({ picture, text = '' }) {
+    const location = useLocation()
+
+    function bannerHeight() {
+        return {
+            height: location.pathname.startsWith('/details')
+                ? '416px'
+                : '224px',
+        }
+    }
+
     return (
-        <div className="banner">
+        <div className="banner" style={bannerHeight()}>
             <img src={picture} alt="banner" />
-            <h1>{text}</h1>
+            {text !== '' && <h1>{text}</h1>}
         </div>
     )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,17 @@
+import { useParams, useLocation } from 'react-router-dom'
+import picture from '../../assets/home_picture.png'
+import Banner from '../../components/Banner'
+
 function Component() {
+    const { id } = useParams()
+    const location = useLocation()
+
     return (
         <div style={{ height: '768px' }}>
-            <h1>Fiche Logement 📔</h1>
+            <h1>
+                Fiche Logement 📔 {id} {location.pathname}
+            </h1>
+            <Banner picture={picture} text={location.pathname} />
         </div>
     )
 }
