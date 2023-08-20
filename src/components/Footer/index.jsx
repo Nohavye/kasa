eb90cb0fd3e923418cc8dd8b125b9d72f4478325
Nohavye <noha.poncelet@gmail.com>
2023-08-20 12:33:00 +0200

    Gestion de la position du footer fonction de la taille de la page.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
 import './styles.scss'
 
 function Component() {
+    const [footerDown, setFooterDown] = useState()
+
+    useEffect(() => {
+        setFooterDown(document.body.clientHeight < window.innerHeight - 210)
+        const handleBodyResize = () => {
+            setFooterDown(document.body.clientHeight < window.innerHeight - 210)
+        }
+        window.addEventListener('resize', handleBodyResize)
+        document.body.addEventListener('click', handleBodyResize)
+        return () => {
+            window.removeEventListener('resize', handleBodyResize)
+            document.body.removeEventListener('click', handleBodyResize)
+        }
+    }, [])
+
     return (
-        <footer>
+        <footer style={{ bottom: footerDown ? '0' : 'auto' }}>
             <img alt="Kasa" />
             <p>© 2020 Kasa. All rights reserved</p>
         </footer>
